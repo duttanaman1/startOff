a8b6b1d0a27717fd8eaf627ecf6ad100b0384fc7
Naman Dutta <37544286+duttanaman1@users.noreply.github.com>
2021-01-18 15:13:53 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.listen(port, () => {
   console.log("Server is up");
 });
